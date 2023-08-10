64268dbacaf4227594cf00e4b0d9e7881a05528c
Imran Hossain <imranhossainsakil22@.com>
2023-08-10 09:50:34 +0600

    Problem two solution added

--- assignment-solution.js
+++ assignment-solution.js
@@ -1,3 +1,5 @@
+// Problem 1 Solution
+
 function cubeNumber(number) {
     if (typeof number !== 'number'){
         return "Please enter a number......"
@@ -8,4 +10,29 @@ function cubeNumber(number) {
 
 let inputNumber = '2';
 let output = cubeNumber(inputNumber);
-console.log(output);
+// console.log(output);
+
+
+// Problem 2 solution
+
+function matchFinder(string1, string2) {
+
+    if( typeof string1 !== 'string' || typeof string2 !== 'string' ){
+        return 'Please provide two string.....';
+    }
+
+    else if (string1.includes(string2)){
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
+
+const str1 = 'john doe'
+const str2 = 'e';
+
+const output2 = matchFinder(str1, str2);
+console.log(output2);
+
